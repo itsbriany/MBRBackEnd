8319c8a8d0189d33dcbe72739aa2b0a880537b50
Brian Yip <itsbriany@gmail.com>
2016-03-27 11:35:42 -0300

    Embedded employment and insurance quote in the mortgage application data model

--- project/common/models/mortgage-application.js
+++ project/common/models/mortgage-application.js
@@ -12,6 +12,11 @@ module.exports = (MortgageApplication) => {
     next();
   });
 
+  MortgageApplication.beforeRemote('find', (ctx, unused, next) => {
+    logger.log('info', 'Request to find mortgage applications...');
+    next();
+  });
+
   // Send back the Mortgage ID
   // MortgageApplication.afterRemote('create', (ctx, next) => {
   //   next(ctx.args.data.mortgageID);
@@ -21,6 +26,11 @@ module.exports = (MortgageApplication) => {
   MortgageApplication.afterRemoteError('create', (ctx, next) => {
     logger.log('error', ctx.error.message);
     next(new Error('The mortgage application already exists'));
+  });
+
+  MortgageApplication.afterRemoteError('find', (ctx, next) => {
+    logger.log('error', ctx.error.message);
+    next(ctx.error);
   });
 
 };
